493c713272ff20f18db6588b70d5943d514a58fa
Ronald Fung <rfung.dev@icloud.com>
2023-09-21 20:30:34 +0100

    Removed test packages in requirements.txt

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,1 @@
-# placeholder file to modularize (to package) .py files
-# under the directory "tests", making the files importable.
+# placeholder file to modularize (to package) .py files under the directory "tests", making the files importable.
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,12 +1,12 @@
 import unittest
-from unittest.mock import patch
 from funcs import func
+
 
 class Test(unittest.TestCase):
 
     def test_sum(self):
-        self.assertEqual(func.sum(2,3), 5)
+        self.assertEqual(func.sum(2, 3), 5)
+
 
 if __name__ == "__main__":
     unittest.main()
-    
