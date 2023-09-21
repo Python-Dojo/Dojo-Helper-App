112fbc06af532a14dc7d9a156329c6bc51009261
Ronald Fung <rfung.dev@icloud.com>
2023-09-21 20:05:50 +0100

    Removed function and test for testing flake8

--- funcs/func.py
+++ funcs/func.py
@@ -2,11 +2,5 @@ def sum(x: float, y: float) -> float:
     return x + y
 
 
-# def misformatted_func(
-# var1, var2, var3
-# ):
-#     print(var1, var2, var3)
-
-
 def corr_formatted_func(var1, var2, var3):
     print(var1, var2, var3)
